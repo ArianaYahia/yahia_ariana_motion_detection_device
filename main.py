f8be32416dec8c25b166fb32ba792d7abe1dae94
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2021-02-01 19:55:53 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def on_forever():
-    pass
-forever(on_forever)
+while True:
+    print(input.acceleration(Dimension.X))
+    if input.acceleration(Dimension.X) > 17 or input.acceleration(Dimension.X) < -17:
+        light.clear()
+    else:
+        light.show_animation(light.rainbow_animation, 100)
+        music.magic_wand.play_until_done()
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-forever(function on_forever() {
+while (true) {
+    console.log(input.acceleration(Dimension.X))
+    if (input.acceleration(Dimension.X) > 17 || input.acceleration(Dimension.X) < -17) {
+        light.clear()
+    } else {
+        light.showAnimation(light.rainbowAnimation, 100)
+        music.magicWand.playUntilDone()
+    }
     
-})
+}
